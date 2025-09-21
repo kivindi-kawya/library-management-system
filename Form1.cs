bf5996bca6db41c924ebe490da64a9ace7cee01f
kivindi-kawya <kivindidesilva@gmail.com>
2025-09-21 15:18:51 +0530

    Init commit

--- Form1.cs
+++ Form1.cs
@@ -8,43 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Library_Manegment_System
+namespace Library_Management_System
 {
-    public partial class Form1 : Form
+    public partial class Form1: Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void btnViewBooks_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("View Books Form Opened");
-        }
-
-        private void btnAddBook_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Add Book Form Opened");
-        }
-
-        private void btnIssueBook_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Issue Book Form Opened");
-        }
-
-        private void btnReturnBook_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Return Book Form Opened");
-        }
-
-        private void btnStudents_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Student Form Opened");
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Library Manegment System")]
+[assembly: AssemblyTitle("Library Management System")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Library Manegment System")]
+[assembly: AssemblyProduct("Library Management System")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dcf18e1a-587f-49fc-86c3-254368ce72eb")]
+[assembly: Guid("dde7411a-dd14-45db-b700-853bebdc133c")]
 
 // Version information for an assembly consists of the following four values:
 //
